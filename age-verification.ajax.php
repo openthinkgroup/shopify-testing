15f369ce7c73fe75ee811b4c3e939d8c3679860a
openthinkgroup <thinktank@openthinkgroup.com>
2015-08-24 17:23:59 +0530

    Update age-verification.ajax.php

--- age-verification.ajax.php
+++ age-verification.ajax.php
@@ -1,24 +1,29 @@
 <?php 
 
-$db = new Mysqli("localhost", "webnweb_shopify", "shopify!@#", "webnweb_shopify_app");
+$host        = "host=ec2-54-243-132-114.compute-1.amazonaws.com";
+   $port        = "port=5432";
+   $dbname      = "dbname=dfjdld5mdvpnbt";
+   $credentials = "user=scvoxznmcyyyvn password=G9rQkI4BZs82-U2BOyAVgJ2h-Z";
+   $db = pg_connect( "$host $port $dbname $credentials"  );
+  
 
 if(isset($_REQUEST['sid'])){
 
 	$sid = $_REQUEST['sid'];
 	
-	if($db->connect_errno){
-
-		die('Connect Error: ' . $db->connect_errno);
-		
-	}
-
-	$sql = "select heading, store_name, message_text, Button1_text, Button1_color, Button2_text, Button2_color from tbl_usersettings where access_token = '$sid'";
+ if(!$db){
+      echo "Error : Unable to open database\n";
+   }
+$sql =<<<EOF
+      select heading, store_name, message_text, Button1_text, Button1_color, Button2_text, Button2_color from tbl_usersettings where access_token = '$sid';
+EOF;
+	//$sql = "select heading, store_name, message_text, Button1_text, Button1_color, Button2_text, Button2_color from tbl_usersettings where access_token = '$sid'";
 	
-	$qry = mysqli_query($db, $sql);
+ $qry = pg_query($db, $sql);
 	
 	if ($qry) {
 		
-		$response = mysqli_fetch_assoc($qry);
+		$response = pg_fetch_assoc($qry);
 		
 		echo $data = json_encode($response);
 		
