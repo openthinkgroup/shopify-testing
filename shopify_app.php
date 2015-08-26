7db0fc91388cdaf889b170181ef8a8aa0e9b311f
openthinkgroup <thinktank@openthinkgroup.com>
2015-08-26 15:47:01 +0530

    Update shopify_app.php

--- shopify_app.php
+++ shopify_app.php
@@ -4,29 +4,34 @@ use sandeepshetty\shopify_api;
 ?>
  <?php
 session_start(); //start a session
+$host        = "host=ec2-54-243-132-114.compute-1.amazonaws.com";
+   $port        = "port=5432";
+   $dbname      = "dbname=dfjdld5mdvpnbt";
+   $credentials = "user=scvoxznmcyyyvn password=G9rQkI4BZs82-U2BOyAVgJ2h-Z";
+   $db = pg_connect( "$host $port $dbname $credentials"  );
+   if(!$db){
+      echo "Error : Unable to open database\n";
+   } 
+//$db = new Mysqli("localhost", "webnweb_shopify", "shopify!@#", "webnweb_shopify_app");
 
-$db = new Mysqli("localhost", "webnweb_shopify", "shopify!@#", "webnweb_shopify_app");
-
-if($db->connect_errno){
-  die('Connect Error: ' . $db->connect_errno);
-}
-
-$select_settings = $db->query("SELECT * FROM tbl_appsettings WHERE id = 1");
+$sql =<<<EOF SELECT * FROM tbl_appsettings WHERE id = 1 EOF;
+$select_settings = pg_query($db, $sql);
+//$select_settings = $db->query("SELECT * FROM tbl_appsettings WHERE id = 1");
 $app_settings = $select_settings->fetch_object();
 
 if(!empty($_GET['shop'])){ //check if the shop name is passed in the URL
   $shop = $_GET['shop']; //shop-name.myshopify.com
   
-  $select_store = $db->query("SELECT store_name FROM tbl_usersettings WHERE store_name = '$shop'"); //check if the store exists
+  $select_store = pg_query($db,"SELECT store_name FROM tbl_usersettings WHERE store_name = '$shop'"); //check if the store exists
   
- if($select_store->num_rows > 0){
+ if($select_store->pg_num_rows > 0){
       
       if(shopify_api\is_valid_request($_GET, $app_settings->shared_secret)){ //check if its a valid request from Shopify        
           $_SESSION['shopify_signature'] = $_GET['signature'];
           $_SESSION['shop'] = $shop;
          //header('Location: http://webandweb.in/shopify_testing/admin.php'); //redirect to the admin page 
          ?>
-         <script>window.location.href = 'http://webandweb.in/shopify_testing/admin.php?shop=<?php echo $_SESSION['shop'] ; ?>';
+         <script>window.location.href = 'https://shopify-testing-app.herokuapp.com/admin.php?shop=<?php echo $_SESSION['shop'] ; ?>';
     </script>
      <?php }
       
@@ -61,7 +66,7 @@ if(!empty($_GET['shop'])&& !empty($_GET['code'])){
   );
  
   //save the shop details to the database
-  $db->query("
+  pg_query($db,"
      INSERT INTO tbl_usersettings 
      SET access_token = '$access_token',
      store_name = '$shop'
@@ -73,7 +78,7 @@ if(!empty($_GET['shop'])&& !empty($_GET['code'])){
 
   // header('location: http://webandweb.in/shopify_testing/admin.php'); 
   ?>
-  <script>window.location.href = 'http://webandweb.in/shopify_testing/admin.php?shop=<?php echo $_SESSION['shop'] ; ?>';
+  <script>window.location.href = 'https://shopify-testing-app.herokuapp.com/admin.php?shop=<?php echo $_SESSION['shop'] ; ?>';
     </script>
   <?php
 }
