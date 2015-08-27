377bda9a31eef0a9ed98bb11e3f6b537e127d056
openthinkgroup <thinktank@openthinkgroup.com>
2015-08-27 19:03:32 +0530

    Update admin.php

--- admin.php
+++ admin.php
@@ -8,52 +8,96 @@
 </head>
 <body>
 <div class="container">
-<div class="hero-unit">
-<h1>Age Verification Script</h1>
-<hr>
-</div>
-<div class="row">
-<h3>Please Fill the form Fields</h3>
-<form action="#" method="post" class="age_form">
-<table>
-<tr>
-<td><label for="Heading">Heading title</label></td>
-<td><input type="text" name="Heading_text" class="Heading_text" required="required"></td>
-</tr>
-<tr>
-<td><label for="message">message</label></td>
-<td><textarea name="message_text" class="message_text" required="required" ></textarea></td>
-</tr>
-<tr>
-<td><label for="Button1">Button 1 Text</label></td>
-<td><input type="text" name="Button1_text" class="Button1_text" required="required"></td>
-</tr>
-<tr>
-<td><label for="Button1_color">Button 1 Background Color</label></td>
-<td><input type="text" name="Button1_color" class="Button1_color" required="required"></td>
-</tr>
-<tr>
-<td><label for="Button2">Button 1 Text</label></td>
-<td><input type="text" name="Button2_text" class="Button2_text" required="required"></td>
-</tr>
-<tr>
-<td><label for="Button2_color">Button 2 Background Color</label></td>
-<td><input type="text" name="Button2_color" class="Button2_color" required="required"></td>
-</tr>
-<tr>
-<td><label for="background_image">Main Background Image</label></td>
-<td><input type="file" name="background_image" class="background_image"></td>
-</tr>
-<tr>
-<td><label for="cookie_lifetime">Cookie Lifetime</label></td>
-<td><input type="text" name="cookie_lifetime" class="cookie_lifetime" style="max-width: 100px;"/>&nbsp;days&nbsp;&nbsp;<code style="color:red">[Default will be 365 days]</code></td>
-</tr>
-<tr>
-<td colspan="2"><input type="submit" class="btn" name="btn"></td>
-</tr>
-</table>
-</form>
-</div>
+	<div class="hero-unit">
+		<h1>Age Verification Script</h1>
+		<hr>
+	</div>
+	<div class="row">
+		<h3>Please Fill the form Fields</h3>
+		<form action="#" method="post" class="age_form">
+			<?php 
+			$sql_get_shop_temp = "SELECT * FROM tbl_usersettings WHERE store_name = '$store'";
+			$res_get_shop_temp = pg_query($db, $sql_get_shop_temp);
+			if($res_get_shop_temp && pg_num_rows($res_get_shop_temp) > 0){ 
+				$data = pg_fetch_assoc($res_get_shop); ?>
+				<table>
+					<tr>
+						<td><label for="Heading">Heading title</label></td>
+						<td><input type="text" name="Heading_text" class="Heading_text" value="<?php echo $data['Heading_text']; ?>" required="required"></td>
+					</tr>
+					<tr>
+						<td><label for="message">message</label></td>
+						<td><textarea name="message_text" class="message_text" value="<?php echo $data['message_text']; ?>" required="required" ></textarea></td>
+					</tr>
+					<tr>
+						<td><label for="Button1">Button 1 Text</label></td>
+						<td><input type="text" name="Button1_text" class="Button1_text" value="<?php echo $data['Button1_text']; ?>" required="required"></td>
+					</tr>
+					<tr>
+						<td><label for="Button1_color">Button 1 Background Color</label></td>
+						<td><input type="text" name="Button1_color" class="Button1_color" value="<?php echo $data['Button1_color']; ?>" required="required">&nbsp;&nbsp;<code style="color:red">[eg: #000000 or black]</code></td>
+					</tr>
+					<tr>
+						<td><label for="Button2">Button 2 Text</label></td>
+						<td><input type="text" name="Button2_text" class="Button2_text" value="<?php echo $data['Button2_text']; ?>" required="required"></td>
+					</tr>
+					<tr>
+						<td><label for="Button2_color">Button 2 Background Color</label></td>
+						<td><input type="text" name="Button2_color" class="Button2_color" value="<?php echo $data['Button2_color']; ?>" required="required">&nbsp;&nbsp;<code style="color:red">[eg: #ff0000 or red]</code></td>
+					</tr>
+					<tr>
+						<td><label for="background_image">Main Background Image</label></td>
+						<td><input type="file" name="background_image" class="background_image"></td>
+					</tr>
+					<tr>
+						<td><label for="cookie_lifetime">Cookie Lifetime</label></td>
+						<td><input type="text" name="cookie_lifetime" class="cookie_lifetime" style="max-width: 100px;" value="<?php echo $data['cookie_lifetime']; ?>"/>&nbsp;days&nbsp;&nbsp;<code style="color:red">[Default will be 365 days]</code></td>
+					</tr>
+					<tr>
+						<td colspan="2"><input type="submit" class="btn" name="btn"></td>
+					</tr>
+				</table>
+			<?php } else {?>
+				<table>
+					<tr>
+						<td><label for="Heading">Heading title</label></td>
+						<td><input type="text" name="Heading_text" class="Heading_text" required="required"></td>
+					</tr>
+					<tr>
+						<td><label for="message">message</label></td>
+						<td><textarea name="message_text" class="message_text" required="required" ></textarea></td>
+					</tr>
+					<tr>
+						<td><label for="Button1">Button 1 Text</label></td>
+						<td><input type="text" name="Button1_text" class="Button1_text" required="required"></td>
+					</tr>
+					<tr>
+						<td><label for="Button1_color">Button 1 Background Color</label></td>
+						<td><input type="text" name="Button1_color" class="Button1_color" required="required">&nbsp;&nbsp;<code style="color:red">[eg: #000000 or black]</code></td>
+					</tr>
+					<tr>
+						<td><label for="Button2">Button 1 Text</label></td>
+						<td><input type="text" name="Button2_text" class="Button2_text" required="required"></td>
+					</tr>
+					<tr>
+						<td><label for="Button2_color">Button 2 Background Color</label></td>
+						<td><input type="text" name="Button2_color" class="Button2_color" required="required">&nbsp;&nbsp;<code style="color:red">[eg: #ff0000 or red]</code></td>
+					</tr>
+					<tr>
+						<td><label for="background_image">Main Background Image</label></td>
+						<td><input type="file" name="background_image" class="background_image"></td>
+					</tr>
+					<tr>
+						<td><label for="cookie_lifetime">Cookie Lifetime</label></td>
+						<td><input type="text" name="cookie_lifetime" class="cookie_lifetime" style="max-width: 100px;"/>&nbsp;days&nbsp;&nbsp;<code style="color:red">[Default will be 365 days]</code></td>
+					</tr>
+					<tr>
+						<td colspan="2"><input type="submit" class="btn" name="btn"></td>
+					</tr>
+				</table>
+			<?php } ?>
+		</form>
+	</div>
 
 <?php 
 
@@ -92,7 +136,7 @@ if (isset($_POST['btn'])){
 	// check first time installation of app
 	$store = $_REQUEST['shop'];
 	$sid = md5(time());
-	$sql_get_shop = "SELECT access_token FROM tbl_usersettings WHERE store_name = '$store'";
+	$sql_get_shop = "SELECT * FROM tbl_usersettings WHERE store_name = '$store'";
 	$res_get_shop = pg_query($db, $sql_get_shop);
 	if($res_get_shop && pg_num_rows($res_get_shop) > 0){ // store settings already exists (update them)
 		$response = pg_fetch_assoc($res_get_shop);
