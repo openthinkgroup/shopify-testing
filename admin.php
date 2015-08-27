4ebb58f16053e1792cea5761996a51b1fe61abb3
openthinkgroup <thinktank@openthinkgroup.com>
2015-08-27 19:15:58 +0530

    Update admin.php

--- admin.php
+++ admin.php
@@ -27,12 +27,8 @@ if(!$db){
 			$store = $_REQUEST['shop'];			
 			$sql_get_shop_temp = "SELECT * FROM tbl_usersettings WHERE store_name = '$store'";
 			$res_get_shop_temp = pg_query($db, $sql_get_shop_temp);
-			$data = pg_fetch_assoc($res_get_shop_temp);
-			echo "<pre>";
-			print_r($data);
-			if($res_get_shop_temp && pg_num_rows($res_get_shop_temp) > 0){ 
-			die('1');
-				$data = pg_fetch_assoc($res_get_shop); ?>
+			if($res_get_shop_temp && pg_num_rows($res_get_shop_temp) > 0){
+				$data = pg_fetch_assoc($res_get_shop_temp); ?>
 				<table>
 					<tr>
 						<td><label for="Heading">Heading title</label></td>
@@ -70,7 +66,7 @@ if(!$db){
 						<td colspan="2"><input type="submit" class="btn" name="btn" value="Generate Script"></td>
 					</tr>
 				</table>
-			<?php } else { die('2'); ?>
+			<?php } else { ?>
 				<table>
 					<tr>
 						<td><label for="Heading">Heading title</label></td>
